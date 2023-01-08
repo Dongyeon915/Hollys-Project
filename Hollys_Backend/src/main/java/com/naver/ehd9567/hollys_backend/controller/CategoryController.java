ffab9d50e927ca16ec52828636db110bf680afaa
dongyeonkim <ehd9567@naver.com>
2023-01-08 23:39:51 +0900

    모든 카테고리를 조회하는 getAllCategory생성

--- Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/controller/CategoryController.java
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package com.naver.ehd9567.hollys_backend.controller;
 
 import com.naver.ehd9567.hollys_backend.dao.CategoryDAO;
+import com.naver.ehd9567.hollys_backend.dto.CategoryDTO;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,11 @@ public class CategoryController {
 
   @Autowired
   private CategoryDAO categoryDAO;
+
+  @GetMapping("/allcategory")
+  public List getAllCategory(){
+    return categoryDAO.getAllCategory();
+  }
+
 
 }
--- Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dao/CategoryDAO.java
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dao/CategoryDAO.java
@@ -1,8 +1,11 @@
 package com.naver.ehd9567.hollys_backend.dao;
 
+import com.naver.ehd9567.hollys_backend.dto.CategoryDTO;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CategoryDAO {
 
+  public List<CategoryDTO> getAllCategory();
 }
