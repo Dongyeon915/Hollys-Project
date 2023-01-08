19a08f029534fd2774b60d3cabf6389e6b6cfc6d
dongyeonkim <ehd9567@naver.com>
2023-01-08 22:51:07 +0900

    DTO클래스 이름 수정

--- Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dto/MenuDTO.java
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dto/MenuDTO.java
@@ -3,7 +3,7 @@ package com.naver.ehd9567.hollys_backend.dto;
 import lombok.Data;
 
 @Data
-public class Menu {
+public class MenuDTO {
   private Integer menu_id;
   private String name;
 }
--- Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dto/ProductDTO.java
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dto/ProductDTO.java
@@ -3,7 +3,7 @@ package com.naver.ehd9567.hollys_backend.dto;
 import lombok.Data;
 
 @Data
-public class Product {
+public class ProductDTO {
   private Integer id;
   private String name;
   private String name_eng;
