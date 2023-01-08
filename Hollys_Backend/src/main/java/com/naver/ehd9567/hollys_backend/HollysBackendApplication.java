46c014bb7dd97d6dc2f8802e451773f5da270728
dongyeonkim <ehd9567@naver.com>
2023-01-08 17:56:26 +0900

    Hollys 프로젝트 추가

---
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/HollysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.naver.ehd9567.hollys_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollysBackendApplication.class, args);
+	}
+
+}
---
+++ Hollys_Backend/src/test/java/com/naver/ehd9567/hollys_backend/HollysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naver.ehd9567.hollys_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollysBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
