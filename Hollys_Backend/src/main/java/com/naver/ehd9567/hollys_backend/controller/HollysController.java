7f2fbed1923a10eaba04465f4d726b2acbf8a8c9
dongyeonkim <ehd9567@naver.com>
2023-01-08 18:37:00 +0900

    hollys 내용 추가

---
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/controller/HollysController.java
@@ -0,0 +1,16 @@
+package com.naver.ehd9567.hollys_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hollys")
+public class HollysController {
+
+  @GetMapping
+  public String hellohollys(){
+    return "안녕 할리스";
+  }
+}
