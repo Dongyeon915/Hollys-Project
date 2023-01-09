f31e4c75d8e06840d55857b4e0a43519492df3af
juyonglee <juyonglee0208@gmail.com>
2023-01-09 17:51:47 +0900

    MenuDTO 문서 추가

--- Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dto/MenuDTO.java
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dto/MenuDTO.java
@@ -1,9 +1,13 @@
 package com.naver.ehd9567.hollys_backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema(description = "메뉴를 나타내는 Class를 의미합니다.")
 @Data
 public class MenuDTO {
-  private Integer menu_id;
-  private String name;
+    @Schema(description = "메뉴의 ID를 의미합니다.", example = "1")
+    private Integer menu_id;
+    @Schema(description = "메뉴명을 의미합니다.", example = "Coffee")
+    private String name;
 }
