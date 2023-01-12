35f8b4ff430784d3fb440ada1dc9a9682d50ef5c
dongyeonkim <ehd9567@naver.com>
2023-01-12 11:09:07 +0900

    Hollys getAll 메서드 생성중

--- Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/controller/HollysController.java
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/controller/HollysController.java
@@ -1,7 +1,17 @@
 package com.naver.ehd9567.hollys_backend.controller;
 
 
+import com.naver.ehd9567.hollys_backend.dao.CategoryDAO;
+import com.naver.ehd9567.hollys_backend.dao.HollysDAO;
+import com.naver.ehd9567.hollys_backend.dao.MenuDAO;
+import com.naver.ehd9567.hollys_backend.dao.ProductDAO;
 import io.swagger.v3.oas.annotations.Operation;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hollys")
 public class HollysController {
 
+  @Autowired
+  HollysDAO hollysDAO;
+  CategoryDAO categoryDAO;
+  MenuDAO menuDAO;
+  ProductDAO productDAO;
+
   @Operation(summary = "Hollys 메인 화면.",
       description = "Hollys의 메인 화면."
   )
@@ -17,4 +33,5 @@ public class HollysController {
   public String helloHollys() {
     return "Hello, Hollys!";
   }
+
 }
---
+++ Hollys_Backend/src/main/java/com/naver/ehd9567/hollys_backend/dao/HollysDAO.java
@@ -0,0 +1,10 @@
+package com.naver.ehd9567.hollys_backend.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HollysDAO {
+
+//  public List<Object> getAll();
+}
